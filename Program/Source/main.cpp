0c509746cf70c1813daeb00f2c84e53b5eb0f298
kunosyn <kunosyn@gmail.com>
2024-08-07 16:26:15 -0500

    refactor: switch to new include type
    Migrate from relative include type.

---
+++ Program/Source/main.cpp
@@ -0,0 +1,16 @@
+#include <Classes/Game.h>
+
+#include <imgui.h>
+#include <imgui-SFML.h>
+#include <imgui-SFML_export.h>
+
+int main (int argc, char* argv[]) {
+	Game game;
+
+	while (game.running()) {
+		game.update();
+		game.render();
+	}
+	
+	return 0;
+}
